ef2cd04b901c51bee4c86b73b926ab86a01408b1
luuckx2 <contato@humorceego.com>
2014-11-24 07:47:03 -0200

    first commit

---
+++ app/controllers/admin/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+class DashboardController extends Controller {
+
+	/**
+	 * Setup the layout used by the controller.
+	 *
+	 * @return void
+	 */
+	protected function index()
+	{
+		return "Ola Admin";
+	}
+
+
+}
---
+++ app/routes.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', function(){
+	return View::make('hello');
+});
+
+Route::get('login', function(){
+    return View::make('admin.login');
+});
+
+Route::get('admin/logout', function(){
+    return Auth::logout();
+    return Redirect::to('admin/login');
+});
+
+Route::post('admin/login', array('uses' => 'HomeController@doLogin'));
+
+Route::group(array('before' => 'auth'), function(){
+		// main page for the admin section (app/views/admin/dashboard.blade.php)
+		Route::get('/admin', function(){
+			
+			return View::make('admin.dashboard');
+			
+		});
+
+});
+
+Route::filter('auth', function()
+{
+    if (Auth::guest()) return Redirect::to('admin/login');
+});
+
+/*Route::post('admin/login', function(){
+    $userdata = array('username' => Input::get('username'),
+                      'password' => Input::get('password'));
+
+    if (Auth::attempt($userdata)){
+        return Redirect::to('admin');
+    }
+    else{
+        return Redirect::to('admin/login')->with('login_errors',true);
+    }
+});*/
+
+/*
+
+
+
+
+Route::controller('admin.dashboard');
+
+Route::get('admin', array('before' => 'auth', function() {
+    return Redirect::to_action('admin@dashboard');
+}));
+
+*/
